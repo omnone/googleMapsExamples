1262568cf81f94ccd8cab5090f9ffe98d6aecf5b
Kostas Bourantas <bourantas@ceid.upatras.gr>
2018-03-17 20:51:30 +0200

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,10 +1,11 @@
 window.initMap = function() {
-
+    //set the default center of the map
     var myLatlng = new google.maps.LatLng(37.983810, 23.727539);
     var mapOptions = {
         zoom: 4,
         center: myLatlng
     }
+    
     var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     // Place a draggable marker on the map
@@ -16,20 +17,25 @@ window.initMap = function() {
     });
 
 
-
+   
     var geocoder = new google.maps.Geocoder();
     var infowindow = new google.maps.InfoWindow;
     var adress_find = "";
-
+    
+    //get the address from the markers data
     function geocodeLatLng(geocoder, map, infowindow, marker) {
         var lat = marker.getPosition().lat();
         var lng = marker.getPosition().lng();
+        
         var input = lat + "," + lng;
+        
         var latlngStr = input.split(',', 2);
         var latlng = { lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1]) };
+        
         geocoder.geocode({ 'location': latlng }, function(results, status) {
             if (status === 'OK') {
                 if (results[0]) {
+                    //print the address you found
                     document.getElementById('current').innerHTML = results[0].formatted_address;
                     adress_find = results[0].formatted_address;
                     document.getElementById("address").value = adress_find;
@@ -45,7 +51,8 @@ window.initMap = function() {
             }
         });
     }
-
+    
+    //get the address and find the place on the map
     function geocodeAddress(geocoder, marker, map) {
         var address = document.getElementById('address').value;
         geocoder.geocode({ 'address': address }, function(results, status) {
@@ -63,11 +70,9 @@ window.initMap = function() {
     }
 
 
-
+    //everytime the marker is dragged find the new address
     google.maps.event.addListener(marker, 'dragend', function(evt) {
         geocodeLatLng(geocoder, map, infowindow, marker);
-
-
     });
 
     document.getElementById('submit').addEventListener('click', function() {
