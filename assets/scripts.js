27ede3c9c4789cdfe6678adb8a1eeff060dc0898
Kostas Bourantas <bourantas@ceid.upatras.gr>
2018-03-28 15:54:51 +0300

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,6 +3,7 @@ function gm_authFailure() {
   alert("Did you change the API key?\n\n"+"Please check index.html");
 };
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 window.initMap = function() {
     //set the default center of the map
@@ -22,14 +23,16 @@ window.initMap = function() {
         title: "Drag me!"
     });
 
-
-   
     var geocoder = new google.maps.Geocoder();
     var infowindow = new google.maps.InfoWindow;
     var adress_find = "";
+  
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     //get the address from the markers data
+    //This function takes lat and lng from marker and translates it to an address
     function geocodeLatLng(geocoder, map, infowindow, marker) {
+      
         var lat = marker.getPosition().lat();
         var lng = marker.getPosition().lng();
         
@@ -46,7 +49,8 @@ window.initMap = function() {
                     adress_find = results[0].formatted_address;
                     document.getElementById("address").value = adress_find;
                     map.setZoom(16);
-
+                     
+                    //update the info window of the marker with the new address
                     infowindow.setContent(results[0].formatted_address);
                     infowindow.open(map, marker);
                 } else {
@@ -57,14 +61,19 @@ window.initMap = function() {
             }
         });
     }
-    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //get the address and find the place on the map
+    //This function takes an address and calculates the lat and lng .
     function geocodeAddress(geocoder, marker, map) {
         var address = document.getElementById('address').value;
         geocoder.geocode({ 'address': address }, function(results, status) {
             if (status === 'OK') {
                 map.setCenter(results[0].geometry.location);
                 document.getElementById('current').innerHTML = results[0].formatted_address;
+              
+                //update the info window of the marker with the new address
+                infowindow.setContent(results[0].formatted_address);
+                infowindow.open(map, marker);
 
                 marker.setPosition(results[0].geometry.location);
                 map.setZoom(16);
@@ -75,12 +84,14 @@ window.initMap = function() {
         });
     }
 
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     //everytime the marker is dragged find the new address
     google.maps.event.addListener(marker, 'dragend', function(evt) {
         geocodeLatLng(geocoder, map, infowindow, marker);
     });
-
+    
+    //on submit find the  new address 
     document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, marker, map);
     });
